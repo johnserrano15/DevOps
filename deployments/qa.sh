084e59bc0c33dcc0481c68bcfcbe65f64d3a1db1
John serrano <andrey_201015@hotmail.com>
2018-05-15 14:50:24 -0500

    mas cambios en la config de circleci agregamos una nueva config

--- deployments/qa.sh
+++ deployments/qa.sh
@@ -1,5 +1,5 @@
 npm install -g now
-sudo npm install -g --unsafe-perm now
+# sudo npm install -g --unsafe-perm now
 echo "deploying..."
 URL=$(now --docker --public -t $now_token)
 echo "running acceptance on $URL"
