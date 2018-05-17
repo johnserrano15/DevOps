df06457143a62e6c9958826ce9e9e0dbbc7641d4
John serrano <andrey_201015@hotmail.com>
2018-05-17 15:15:45 -0500

    Agregando loadbalancer conectadolos con las instancias gracias a los tags

--- infra/main.tf
+++ infra/main.tf
@@ -1,3 +1,23 @@
+resource "digitalocean_loadbalancer" "devopsloadbal" {
+  name = "devops-html-v2"
+  region = "nyc3"
+
+  forwarding_rule {
+    entry_port = 80
+    entry_protocol = "http"
+
+    target_port = 3000
+    target_protocol = "http"
+  }
+
+  healthcheck {
+    port = 3000
+    protocol = "http"
+  }
+
+  droplet_tag = "${digitalocean_tag.devopstag.name}"
+}
+
 resource "digitalocean_tag" "devopstag" {
   name = "devops-html"
 }
