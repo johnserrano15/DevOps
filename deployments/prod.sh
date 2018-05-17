4cc4297232cbf208ef7618e5469550a09b6c942d
John serrano <andrey_201015@hotmail.com>
2018-05-17 17:40:58 -0500

    Cambios al prod.sh

--- deployments/prod.sh
+++ deployments/prod.sh
@@ -1,4 +1,5 @@
 wget -O packer https://releases.hashicorp.com/packer/1.2.3/packer_1.2.3_linux_amd64.zip
 unzip packer.zip
+sudo chmod +x packer
 ./packer validate deployments/template.json
 ./packer build deployments/template.json
