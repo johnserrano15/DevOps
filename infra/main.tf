339cc9963d02891f933f1e4089e2d5543861cb21
John serrano <andrey_201015@hotmail.com>
2018-05-17 14:55:57 -0500

    Agregando tags y mas instancias con count

--- infra/main.tf
+++ infra/main.tf
@@ -1,8 +1,14 @@
+resource "digitalocean_tag" "devopstag" {
+  name = "devops-html"
+}
+
 resource "digitalocean_droplet" "web" {
+  count              = 3
   image              = "34398260"
   name               = "devops-v2"
   region             = "nyc3"
   size               = "512mb"
+  tags               = ["${digitalocean_tag.devopstag.id}"] 
   ssh_keys           = [20843332]
   user_data          = "${file("web.conf")}"
 }
