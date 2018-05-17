2d4f48437c8826051483615925b552f3cdaa8db8
John serrano <andrey_201015@hotmail.com>
2018-05-17 14:25:12 -0500

    Resolviendo problema con el #cloud-config

--- infra/main.tf
+++ infra/main.tf
@@ -4,18 +4,5 @@ resource "digitalocean_droplet" "web" {
   region             = "nyc3"
   size               = "512mb"
   ssh_keys           = [20843332]
-  user_data          = <<EOF
-  #cloud-config
-  coreos:
-    units:
-      - name: devopsdemo.service
-        command: start
-        content: |
-          [Unit]
-          Description=devops demo
-          After=docker.service
-
-          [Service]
-          ExecStart=/usr/bin/docker -d -p 3000:3000 devops
-  EOF
+  user_data          = "${file("web.conf")}"
 }
